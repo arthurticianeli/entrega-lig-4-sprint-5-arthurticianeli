7079473a5fb568f914514d0fd43c415827c22f75
ErickDevMaster <123skatesk8@gmail.com>
2021-10-05 08:39:58 -0400

    verificaçao vertical pronta

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ let tabuleiro = [
     // "0212200",
     // "1111210"
     "0000000",
-    "0110000",
-    "0122000",
+    "0120000",
+    "0112100",
     "2211220",
-    "2212221",
-    "1112212"
+    "2212211",
+    "1112211"
 ]
 const criarTabuleiro = () =>{
     for(let i = 0 ; i < tabuleiro.length; i++){
@@ -25,7 +25,7 @@ const criarTabuleiro = () =>{
             }else if(tabuleiro[i][a] === "2"){
                 bloco.classList.add("black")
             }
-            //bloco.textContent = tabuleiro[i][a]
+            // bloco.textContent = tabuleiro[i][a]
             bloco.id = `${i}0${a}`
             divPai.appendChild(bloco)
         }
@@ -43,18 +43,29 @@ const verificaVitoria = (jogador) =>{
     }
     let palavraCruzada = ""
     let palavraCruzadaD = ""
+    let palavraVertical = ""
     for(let i = 0 ; i < tabuleiro.length; i++){
-        for(let a = 0 ; a <= 3 ; a++){
+        for(let a = 0 ; a < tabuleiro[i].length ; a++){
             if(i <= 2){
-                palavraCruzada = tabuleiro[i][a] + tabuleiro[i+1][a+1] + tabuleiro[i+2][a+2] + tabuleiro[i+3][a+3] 
-                palavraCruzadaD = tabuleiro[i][a+3] + tabuleiro[i+1][a+2] + tabuleiro[i+2][a+1] + tabuleiro[i+3][a]
-                console.log(palavraCruzada,palavraCruzadaD)
+                palavraVertical = tabuleiro[i][a] + tabuleiro[i+1][a] + tabuleiro[i+2][a] + tabuleiro[i+3][a]
+                console.log(palavraVertical+" vertical") 
             }
-            if(tabuleiro[i].substr(a, a+4) === condicao || palavraCruzada === condicao || palavraCruzadaD === condicao){
+            if(a <= 3){
+                if(i <= 2){
+                    palavraCruzada = tabuleiro[i][a] + tabuleiro[i+1][a+1] + tabuleiro[i+2][a+2] + tabuleiro[i+3][a+3] 
+                    palavraCruzadaD = tabuleiro[i][a+3] + tabuleiro[i+1][a+2] + tabuleiro[i+2][a+1] + tabuleiro[i+3][a]
+                    // console.log(palavraCruzada,palavraCruzadaD)
+                }
+                if(tabuleiro[i].substr(a, a+4) === condicao || palavraCruzada === condicao || palavraCruzadaD === condicao){
+                    return console.log(`${jogador} Ganhou`)
+                }
+            }
+            if(palavraVertical === condicao){
                 return console.log(`${jogador} Ganhou`)
             }
+            
         }
     }
 }
 
-verificaVitoria("black")
+verificaVitoria("red")
