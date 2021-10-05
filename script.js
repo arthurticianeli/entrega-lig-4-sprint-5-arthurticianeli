176ebdacbb9fbd10a8b0c45b32304b6fbc011015
ErickDevMaster <123skatesk8@gmail.com>
2021-10-05 16:00:15 -0400

    parte funcional pronta

--- script.js
+++ script.js
@@ -8,28 +8,7 @@ let tabuleiro = [
     "0000000",
     "0000000"
 ]
-let arrJogadas = []
-function moverDiscos(event){
-    let colunaClicada = event.currentTarget
-    let classeColunaClicada = colunaClicada.className
-    let posicaoArr = Number(classeColunaClicada[classeColunaClicada.length-1])
-    console.log(classeColunaClicada)
-    console.log(posicaoArr)
-    arrJogadas.push(classeColunaClicada)
-    let disco = document.createElement("div")
-    if(arrJogadas.length === 2 && arrJogadas[0] === arrJogadas[1]){
-        if(tabuleiro[posicaoArr] === "0"){
 
-        }
-        console.log(arrJogadas)
-        arrJogadas = []
-    }else if(arrJogadas[0] !== arrJogadas [1]){
-        let ultimaJogada = arrJogadas[1]
-        arrJogadas = []
-        arrJogadas.push(ultimaJogada)
-    }
-    console.log(event.currentTarget)
-}
 const criarTabuleiro = () =>{
     for(let i = 0 ; i < tabuleiro[0].length; i++){
         let coluna = document.createElement("div")
@@ -46,7 +25,8 @@ const criarTabuleiro = () =>{
             }else if(tabuleiro[a][i] === "2"){
                 bloco.classList.add("black")
             }
-            bloco.textContent = tabuleiro[a][i]
+            // bloco.textContent = tabuleiro[a][i]
+            bloco.id = `${a}0${i}`
             coluna.appendChild(bloco)
             
         }
@@ -54,8 +34,11 @@ const criarTabuleiro = () =>{
     }
 }
 criarTabuleiro()
-
+let jogadas = 0
+let maximoJogadas = (tabuleiro.length * tabuleiro[0].length)
 const verificaVitoria = (jogador) =>{
+    jogadas++
+    console.log(jogadas)
     let condicao = ""
     if(jogador === "red"){
         condicao = "1111"
@@ -65,29 +48,74 @@ const verificaVitoria = (jogador) =>{
     let palavraCruzada = ""
     let palavraCruzadaD = ""
     let palavraVertical = ""
-    for(let i = 0 ; i < tabuleiro.length; i++){
-        for(let a = 0 ; a < tabuleiro[i].length ; a++){
-            if(i <= 2){
-                palavraVertical = tabuleiro[i][a] + tabuleiro[i+1][a] + tabuleiro[i+2][a] + tabuleiro[i+3][a]
-                // console.log(palavraVertical+" vertical") 
-            }
-            if(a <= 3){
+    if(jogadas < maximoJogadas){
+        for(let i = 0 ; i < tabuleiro.length; i++){
+            for(let a = 0 ; a < tabuleiro[i].length ; a++){
                 if(i <= 2){
-                    palavraCruzada = tabuleiro[i][a] + tabuleiro[i+1][a+1] + tabuleiro[i+2][a+2] + tabuleiro[i+3][a+3] 
-                    palavraCruzadaD = tabuleiro[i][a+3] + tabuleiro[i+1][a+2] + tabuleiro[i+2][a+1] + tabuleiro[i+3][a]
-                    // console.log(palavraCruzada,palavraCruzadaD)
+                    palavraVertical = tabuleiro[i][a] + tabuleiro[i+1][a] + tabuleiro[i+2][a] + tabuleiro[i+3][a]
+                    // console.log(palavraVertical+" vertical") 
                 }
-                if(tabuleiro[i].substr(a, a+4) === condicao || palavraCruzada === condicao || palavraCruzadaD === condicao){
+                if(a <= 3){
+                    if(i <= 2){
+                        palavraCruzada = tabuleiro[i][a] + tabuleiro[i+1][a+1] + tabuleiro[i+2][a+2] + tabuleiro[i+3][a+3] 
+                        palavraCruzadaD = tabuleiro[i][a+3] + tabuleiro[i+1][a+2] + tabuleiro[i+2][a+1] + tabuleiro[i+3][a]
+                        // console.log(palavraCruzada,palavraCruzadaD)
+                    }
+                    if(tabuleiro[i].substr(a, a+4) === condicao || palavraCruzada === condicao || palavraCruzadaD === condicao){
+                        return console.log(`${jogador} Ganhou`)
+                    }
+                }
+                if(palavraVertical === condicao){
                     return console.log(`${jogador} Ganhou`)
                 }
+                
             }
-            if(palavraVertical === condicao){
-                return console.log(`${jogador} Ganhou`)
-            }
-            
         }
+    }else{
+        console.log("empatou!")
     }
+    // return console.log("empatou!!")
 }
 
-verificaVitoria("black")
 
+
+
+let arrJogadas = []
+function moverDiscos(event){
+    let colunaClicada = event.currentTarget
+    let classeColunaClicada = colunaClicada.className
+    arrJogadas.push(classeColunaClicada)
+    let disco = document.createElement("div")
+    disco.classList.add("red")
+    if(arrJogadas.length === 2 && arrJogadas[0] === arrJogadas[1]){
+        let posicaoArr = Number(classeColunaClicada[classeColunaClicada.length-1])
+        // console.log(posicaoArr)
+        String.prototype.replaceAt=function(index, replacement) {
+            return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+        }
+        for(let i = tabuleiro.length-1 ; i >= 0 ;i--){
+            let blocoPai = document.getElementById(`${i}0${posicaoArr}`)
+            if(tabuleiro[i][posicaoArr] === "0"){
+                tabuleiro[i] = tabuleiro[i].replaceAt(posicaoArr,"1")
+                blocoPai.appendChild(disco)
+                verificaVitoria("red")
+                // console.log(tabuleiro)
+                // console.log("esta vazio")
+                // console.log(i)
+                i=0
+            }else{
+                console.log("nao esta vazio")
+            }
+        }
+        if(tabuleiro[posicaoArr] === "0"){
+
+        }
+        // console.log(arrJogadas)
+        arrJogadas = []
+    }if(arrJogadas.length >= 2 && arrJogadas[0] !== arrJogadas [1]){
+        let ultimaJogada = arrJogadas[1]
+        arrJogadas = []
+        arrJogadas.push(ultimaJogada)
+    }
+    // console.log(event.currentTarget)
+}
